783b81ccc5a40128b30df578dcceff0f78d78fe3
leonardo-lin <67550587+leonardo-lin@users.noreply.github.com>
2022-11-19 23:27:08 +0800

    Add files via upload

--- segmenter.py
+++ segmenter.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#from udicOpenData.dictionary import *
-#from udicOpenData.stopwords import *
-import jieba
+from udicOpenData.dictionary import *
+from udicOpenData.stopwords import *
+#import jieba
 import re
 
 def segment(sentence, cut_all=False):
     #print(sentence)
     sentence = sentence.replace('\n', '').replace('\u3000', '').replace('\u00A0', '')
     #print(sentence)
-    sentence = ' '.join(jieba.cut(sentence, cut_all=cut_all))
+    #sentence = ' '.join(jieba.cut(sentence, cut_all=cut_all))
     #i=input()
-    #print(sentence)
+    sentence=list(rmsw(sentence,flag=False))
+    #cutword=''
+    #for i in sentence:
+    #    cutword+=i
+    #    cutword+=' '
     
-    
-    return re.sub('[a-zA-Z0-9.。:：,，)）(（！!??”“\"]', '', sentence).split()
+    return sentence
+    #return re.sub('[a-zA-Z0-9.。:：,，)）(（！!??”“\"]', '', sentence).split()
